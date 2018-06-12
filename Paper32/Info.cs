209637582a7e1db23dbad85c72dbd037d30738d0
ahoZiorce <aho@sior.be>
2018-06-12 13:01:25 +0200

    Changed default port to 9033

--- Paper32/Info.cs
+++ Paper32/Info.cs
@@ -14,9 +14,10 @@ namespace Paper32
         static public string DesktopFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\";
         static public string FileDescription = "Paper32";
         static public string CurrentPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
-        static public string Version = "p_102";
+        static public string Version = "p_103";
 
-        static public string URL = "http://localhost";
+        // IMPORTANT : CHANGE THE FOLLOWING ADDRESS TO BE ON THE CORRECT URL OF THE INSTRUCTIONS SERVER
+        static public string URL = "http://localhost:9033";
         static public string Mac = new Regex(@"[^A-Za-z0-9]").Replace(Tools.GetMacAddress(), "");
         static public string User = new Regex(@"[^A-Za-z0-9]").Replace(System.Security.Principal.WindowsIdentity.GetCurrent().Name, "");
         static public string HTTPAuth = "azertyuiop"; // This is a placeholder password. The more important is that the "add instruction" one stays safe.
